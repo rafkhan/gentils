ee5c201f113707f979b2c20492967ffbfc3385ba
Rafael Khan <raf.khan.f@gmail.com>
2012-11-25 20:25:24 -0500

    Bug fixes n shit

--- src/ls/ls.c
+++ src/ls/ls.c
@@ -2,46 +2,50 @@
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
+#include <unistd.h>
 
-#include "ls.h"
+struct arg_list {
+	int show_hidden;
+	char** dir_list;
+};
+
+int one(const struct dirent *ent);
+void itr_ent(char* dir_name, struct arg_list args);
+int no_dot(const struct dirent *ent);
 
 int main(int argc, char** argv) {
 
-	//Points to the locations of directories in argv
-	char** dl = malloc(argc * sizeof(char**));
+	//This stores all the arguments
 	struct arg_list args = {0, NULL};
 
+	// Parse flags and store them in args
+	int c;
+	while((c = getopt(argc, argv, "a")) != -1) {
+		switch(c) {
+			case 'a':
+				args.show_hidden = 1;
+				break;
+		}
+	}
+
+	/*
+	 * Iterate over all args, looking for those
+	 * that do not begin with '-', those are 
+	 * directories that need to be printed out
+	 */
 	int i;
 	int j = 0; //Counts location in dir list
+	char** dl = malloc((argc - 1) * sizeof(char**));
+	if(dl == NULL) exit(1);
 	for(i = 1; i < argc; i++) {
-		//Check for flag
-		if(argv[i][0] == '-') {
-			int n;
-			int slen = strlen(argv[i]);
-			//Skip "-" and move to flag content
-			for(n = 1; n < slen; n++) {
-				switch(argv[i][n]) {
-					// -a: Show .*
-					case 'a':
-						args.show_hidden = 1;
-				}
-			} 
-		} else { //not a flag, therefore its a dir to list
+		//Check for not flag
+		if(argv[i][0] != '-') {
 			dl[j] = argv[i];
 			j++;
 		}
 	}
-	dl[j] = NULL;
-	args.dir_list = dl;
-
-	/*
-	for(i = 0; i < (argc - 1); i++) {
-		char* str = args.dir_list[i];
-		if(str != NULL) {
-			printf("%s\n", str);
-		}
-	}
-	*/
+	dl[j] = NULL; //NULL terminator 
+	args.dir_list = dl; //Set directory list in args
 
 	itr_ent(argv[1], args);
 }
@@ -55,7 +59,7 @@ void itr_ent(char* dir_name, struct arg_list args) {
 	// Iterate over each directory in arg_list
 	int i = 0;
 	while(1) {
-		char* dir = args.dir_list[i];
+		char* dir = args.dir_list[i]; //get curren
 		if(dir == NULL) {
 			if(i == 0) {
 				dir = "./";
